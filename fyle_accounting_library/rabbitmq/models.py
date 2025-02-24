7513424645d73e9a341bf677b69e485f4da524bd
ashwin1111 <winash1998@gmail.com>
2025-02-24 14:23:56 +0530

    add failed events model

---
+++ fyle_accounting_library/rabbitmq/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class FailedEvent(models.Model):
+    """
+    Model to store the failed events
+    """
+    id = models.AutoField(primary_key=True)
+    routing_key = models.CharField(max_length=255)
+    payload = models.JSONField(default=dict)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    error_traceback = models.TextField(null=True)
+    workspace_id = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'failed_events'
