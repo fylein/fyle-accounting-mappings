38cd2b4b05a0297b4783d1d235cd5117b8c5f6c5
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2025-08-07 14:54:29 +0530

    chore: add enum for rabbitmq exchange sage file export (#198)

--- fyle_accounting_library/rabbitmq/enums.py
+++ fyle_accounting_library/rabbitmq/enums.py
@@ -8,3 +8,4 @@ class RabbitMQExchangeEnum:
     NETSUITE_EXCHANGE = 'netsuite_exchange'
     SAGE_DESKTOP_EXCHANGE = 'sage_desktop_exchange'
     QUICKBOOKS_CONNECTOR_EXCHANGE = 'quickbooks_connector_exchange'
+    SAGE_FILE_EXPORT_EXCHANGE = 'sage_file_export_exchange'
